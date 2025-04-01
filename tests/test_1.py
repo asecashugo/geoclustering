f061cb4bc13075db43d15c47a38ab0e85ed82f29
Hugo <asecashugo@gmail.com>
2025-04-01 12:57:16 +0200

    fixed test_1.py: creating output folder during test

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,4 +4,6 @@ N_CLUSTERS=2
 points_gdf=gpd.GeoDataFrame.from_file("input/points.geojson")
 points_gdf=cluster(points_gdf, N_CLUSTERS)
 fig=plot_clusters(points_gdf)
+import os
+os.makedirs("output", exist_ok=True)
 fig.write_html("output/clustered_points.html")
