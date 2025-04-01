0deab3922a709c4afd16d6932428e891f4305939
Hugo <asecashugo@gmail.com>
2025-04-01 12:48:05 +0200

    add pytest for testing and update test_1.py to use new cluster functions

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
-import cluster
+from clustering.cluster import cluster, plot_clusters
 N_CLUSTERS=2
 points_gdf=gpd.GeoDataFrame.from_file("input/points.geojson")
-points_gdf=cluster.cluster(points_gdf, N_CLUSTERS)
-fig=cluster.plot_clusters(points_gdf)
+points_gdf=cluster(points_gdf, N_CLUSTERS)
+fig=plot_clusters(points_gdf)
 fig.write_html("output/clustered_points.html")
