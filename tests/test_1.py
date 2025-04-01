bf923e7a0120dc0c9ee0addbb7812668f091f630
Hugo <asecashugo@gmail.com>
2025-04-01 13:05:42 +0200

    formatted test to be picked up by pytest

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,11 +1,12 @@
-import geopandas as gpd
-from clustering.cluster import cluster, plot_clusters
-N_CLUSTERS=2
-points_gdf=gpd.GeoDataFrame.from_file("input/points.geojson")
-points_gdf=cluster(points_gdf, N_CLUSTERS)
-fig=plot_clusters(points_gdf)
-import os
-os.makedirs("output", exist_ok=True)
-fig.write_html("output/clustered_points.html")
-# assert that output file exists
-assert os.path.exists("output/clustered_points.html")
+def test_html_map():
+
+    import geopandas as gpd
+    from clustering.cluster import cluster, plot_clusters
+    N_CLUSTERS=2
+    points_gdf=gpd.GeoDataFrame.from_file("input/points.geojson")
+    points_gdf=cluster(points_gdf, N_CLUSTERS)
+    fig=plot_clusters(points_gdf)
+    import os
+    os.makedirs("output", exist_ok=True)
+    fig.write_html("output/clustered_points.html")
+    assert os.path.exists("output/clustered_points.html")
